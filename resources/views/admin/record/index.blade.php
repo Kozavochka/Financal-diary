1eaef0aeee5ae507cd146f2ed4fad244f79e561d
Kozavochka <planchet2013@gmail.com>
2023-06-16 13:23:11 +0300

    add some front things

--- resources/views/admin/record/index.blade.php
+++ resources/views/admin/record/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
-<h1 class="mb-3">Остелижвание стоимости</h1>
-
+<h1 class="mb-3">Отслеживание стоимости</h1>
+<h2 class="text-muted">В разработке....</h2>
 <ul class="nav nav-pills nav-fill">
     <li class="nav-item">
         <a class="nav-link" href="{{route('stocks.record.create')}}">Акции</a>
