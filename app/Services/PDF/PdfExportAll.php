082f98af0a9b1527dc7821c5c19020470ba869fd
Kozavochka <planchet2013@gmail.com>
2023-06-21 15:12:34 +0300

    attempt to add css styles to pdf(-)

--- app/Services/PDF/PdfExportAll.php
+++ app/Services/PDF/PdfExportAll.php
@@ -5,6 +5,8 @@ namespace App\Services\PDF;
 use App\Models\Industry;
 use App\Models\Stock;
 use Dompdf\Dompdf;
+use Dompdf\Options;
+use http\Env;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class PdfExportAll
@@ -22,11 +24,16 @@ class PdfExportAll
 
         $total = array_sum($data);
 
-        $pdf = new Dompdf();
+
+        $options = new Options();
+//        $options->setFontDir(storage_path('css')); //?????
+
+
+        $pdf = new Dompdf($options);
         $pdf->loadHtml(view('pdf.general_pdf', compact('data', 'total', 'stocks', 'industries')));
         $pdf->setPaper('A4', 'portrait');
         $pdf->render();
-
+//        dd($pdf);
         return $pdf->stream('general.pdf');
     }
 }
--- resources/views/pdf/general_pdf.blade.php
+++ resources/views/pdf/general_pdf.blade.php
@@ -8,8 +8,6 @@
 
     <title>Отчёт портфеля</title>
 
-    {{--https://fonts.google.com/specimen/Oswald--}}
-
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
         body {
@@ -21,6 +19,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css ')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css ')}}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
