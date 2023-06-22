b98d6a0e670b3acd6c0ed88de71a96f40ad765a1
Andrey <planchet2013@gmail.com>
2023-06-22 17:01:24 +0300

    add bonds to pdf

--- app/Services/PDF/PdfExportAll.php
+++ app/Services/PDF/PdfExportAll.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\PDF;
 
+use App\Models\Bond;
 use App\Models\Industry;
 use App\Models\Stock;
 use Dompdf\Dompdf;
@@ -22,15 +23,20 @@ class PdfExportAll
             ->withCount('stocks')
             ->get();
 
+        $bonds = Bond::query()
+            ->get();
+
         $total = array_sum($data);
+
         $options = new Options();
-        $options->setChroot(__DIR__);
+        $options->setChroot(__DIR__);//Доступ к папке с css
 
         $pdf = new Dompdf($options);
 
-        $pdf->set_base_path( __DIR__ );
+        $pdf->set_base_path( __DIR__ );//Где искать css файлы | поменять!
 
-        $pdf->loadHtml(view('pdf.general_pdf', compact('data', 'total', 'stocks', 'industries')),
+        $pdf->loadHtml(view('pdf.general_pdf', compact('data', 'total', 'stocks', 'industries',
+        'bonds')),
             'UTF-8');
         $pdf->setPaper('A4', 'portrait');
         $pdf->render();
--- resources/views/pdf/general_pdf.blade.php
+++ resources/views/pdf/general_pdf.blade.php
@@ -28,7 +28,7 @@
                 <thead>
                 <tr>
                     <th scope="col">Направление</th>
-                    <th scope="col">Стоимость</th>
+                    <th scope="col">Стоимость RUB</th>
                     <th scope="col">%</th>
                 </tr>
                 </thead>
@@ -36,7 +36,7 @@
                 @foreach($data as $key => $value)
                     <tr>
                         <td>{{$key}}</td>
-                        <td>{{$data[$key]}} RUB</td>
+                        <td>{{$data[$key]}}</td>
                         <td>{{bcdiv($data[$key] / $total * 100,1,0)}}</td>
                     </tr>
                 @endforeach
@@ -87,6 +87,34 @@
                 </tbody>
             </table>
         </div>
+    </div>
+
+    <div class="col-md-8 mb-5">
+        <p class="mb-3 h_text"><b>Облигации</b></p>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Название</th>
+                <th scope="col">Тикер</th>
+                <th scope="col">Купон RUB</th>
+                <th scope="col">%</th>
+                <th scope="col">Дата погашения</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($bonds as $bond)
+                <tr>
+                    <th scope="row">{{$bond->id}}</th>
+                    <td>{{$bond->name}}</td>
+                    <td>{{$bond->ticker}}</td>
+                    <td>{{$bond->coupon}}</td>
+                    <td>{{$bond->profit_percent}}</td>
+                    <td>{{$bond->expiration_date}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 </body>
