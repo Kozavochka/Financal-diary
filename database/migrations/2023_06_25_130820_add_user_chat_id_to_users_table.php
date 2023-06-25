9756ea11917611c5139ce113bf628881eafbd527
Kozavochka <planchet2013@gmail.com>
2023-06-25 20:31:47 +0300

    add nullable to chat_id

--- database/migrations/2023_06_25_130820_add_user_chat_id_to_users_table.php
+++ database/migrations/2023_06_25_130820_add_user_chat_id_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('chat_id')->after('role');
+            $table->unsignedInteger('chat_id')->after('role')->nullable();
         });
     }
 
