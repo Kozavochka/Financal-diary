de90331a809f6fc15c89057f9b75f11e1b6a3168
Andrey <planchet2013@gmail.com>
2023-07-13 17:25:15 +0300

    moved api key to env

--- app/Services/Api/Command/CryptoUpdate.php
+++ app/Services/Api/Command/CryptoUpdate.php
@@ -16,12 +16,14 @@ class CryptoUpdate
 
         $cryptos = Crypto::query()
             ->get();
+
+        $key = env('MY_ALPHAVANTAGE_API_KEY');
         foreach ($cryptos as $crypto){
             $response = $client->get("https://www.alphavantage.co/query?".
                 "function=CURRENCY_EXCHANGE_RATE".
                 "&from_currency={$crypto->ticker}&".
                 "to_currency=USD&".
-                "apikey=DJSG6DBIPC2ERBII");
+                "apikey={$key}");
             $data = json_decode($response->getBody(), true);
             $cryptoPrice = $data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
             /*     dump($crypto->ticker ,$cryptoPrice);*/
