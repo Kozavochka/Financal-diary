fa0c3879dbd9fdb8f63f7263f4409468b8fcbe35
Andrey <planchet2013@gmail.com>
2023-11-22 23:09:10 +0300

    create statistic service

--- app/Http/Controllers/Admin/Statistic/StatisticController.php
+++ app/Http/Controllers/Admin/Statistic/StatisticController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Admin\Statistic;
 
 use App\Http\Controllers\Controller;
 use App\Models\TotalStatistic;
+use App\Services\Statistic\TotalStatisticServiceContract;
 use Illuminate\Http\Request;
 
 class StatisticController extends Controller
 {
+    protected $service;
+
+    public function __construct(TotalStatisticServiceContract $service)
+    {
+        $this->service = $service;
+    }
 
     public function index()
     {
@@ -18,6 +25,7 @@ class StatisticController extends Controller
 
     public function create()
     {
+        //todo получение последней статистики и проверка времени
 
     }
 }
