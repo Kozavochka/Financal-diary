65bff951bc0c2a0a2379e2d67d62af7b0880c091
Andrey <planchet2013@gmail.com>
2024-01-02 19:36:18 +0300

    create statistic's methods and routes

--- app/Http/Controllers/Admin/Statistic/StatisticController.php
+++ app/Http/Controllers/Admin/Statistic/StatisticController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers\Admin\Statistic;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bond;
+use App\Models\Crypto;
+use App\Models\Industry;
+use App\Models\Stock;
 use App\Models\TotalStatistic;
+use App\Services\Chart\DataChartService;
 use App\Services\Statistic\TotalStatisticServiceContract;
 use Carbon\Carbon;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class StatisticController extends Controller
 {
     protected $service;
-
-    public function __construct(TotalStatisticServiceContract $service)
+    protected $chartSerive;
+    public function __construct(TotalStatisticServiceContract $service, DataChartService $chartSerive)
     {
         $this->service = $service;
+        $this->chartSerive = $chartSerive;
     }
 
     public function index()
@@ -21,13 +28,13 @@ class StatisticController extends Controller
         return view('admin.statistic.index');
     }
 
-    public function totalResultStatistic()
+    public function dynamicStatistic()
     {
         $statistics = TotalStatistic::query()->paginate(10);
 
-        return view('admin.statistic.total_result', compact('statistics'));
+        return view('admin.statistic.dynamic', compact('statistics'));
     }
-    public function create()
+    public function createDynamicStatistic()
     {
         $statistic = TotalStatistic::query()->first();
 
@@ -37,5 +44,36 @@ class StatisticController extends Controller
         $this->service->calculate();
 
         return view('admin.statistic.wait');
+    }
+    public function totalStatistic()
+    {
+        $dataArray = $this->chartSerive
+            ->setAssetsData()
+            ->getChartData();
+
+        $dataChart = $dataArray['dataChart'];
+        $data = $dataArray['data'];
+
+        return view('admin.statistic.total', compact('dataChart',  'data'));
+    }
+
+    public function assetsStatistic()
+    {
+        $stocks = QueryBuilder::for(Stock::class)
+            ->orderByRaw('total_price desc')
+            ->get();
+
+        $crypto = Crypto::query()->get();
+
+        $industries = Industry::query()
+            ->withCount('stocks')
+            ->get();
+
+        $bonds = Bond::query()->get();
+
+        return view(
+            'admin.statistic.assets',
+            compact('stocks','crypto','industries','bonds')
+        );
     }
 }
