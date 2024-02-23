e9885d19488eb94b11d3085f76de290a5ea60204
Kozavochka <planchet2013@gmail.com>
2024-02-23 23:40:55 +0300

    refactoring loans forms

--- resources/views/admin/loans/edit.blade.php
+++ resources/views/admin/loans/edit.blade.php
@@ -4,26 +4,29 @@
         @csrf
         @method('patch')
         <div class="form-group">
-            <div class="mb-3">
-                <label for="name" class="form-label">Название</label>
-                <input id="name" name="name"  class="form-control" type="text" placeholder="Введите название"
+        <div class="flex">
+                <div class="mr-2">
+                    <label for="name" class="form-label">Название</label>
+                    <input id="name" name="name"  class="form-control text-input" type="text" placeholder="Введите название"
                        value="{{ old('name', $loan->name) }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="price" class="form-label">Цена</label>
-                <input id="price"  name="price" class="form-control" type="number" step="0.01" placeholder="Введите число"
-                       value="{{ old('price', $loan->price) }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="count_bus" class="form-label">Количество компаний</label>
-                <input id="count_bus"  name="count_bus" class="form-control" type="number" step="0.01" placeholder="Введите число"
-                       value="{{ old('count_bus', $loan->price) }}">
+                </div>
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group">
+            <div class="flex">
+                <div class="mr-2">
+                    <label for="price" class="form-label">Цена</label>
+                    <input id="price"  name="price" class="form-control numeric-input" type="number" step="0.01"
+                       value="{{ old('price', $loan->price) }}">
+                </div>
+                <div class="mr-2">
+                    <label for="count_bus" class="form-label">Количество компаний</label>
+                    <input id="count_bus"  name="count_bus" class="form-control numeric-input" type="number" step="0.01"
+                       value="{{ old('count_bus', $loan->price) }}">
+                </div>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Сохранить</button>
 @endsection
--- resources/views/admin/loans/form.blade.php
+++ resources/views/admin/loans/form.blade.php
@@ -1,24 +1,23 @@
 <div class="form-group">
-    <div class="mb-3">
-        <label for="name" class="form-label">Название</label>
-        <input id="name" name="name"  class="form-control" type="text" placeholder="Введите название" >
+    <div class="flex">
+        <div class="mr-2">
+            <label for="name" class="form-label">Название</label>
+            <input id="name" name="name"  class="form-control text-input" type="text" placeholder="Введите название">
+        </div>
     </div>
 </div>
 
 <div class="form-group">
-    <div class="mb-3">
-        <label for="price" class="form-label">Цена</label>
-        <input id="price"  name="price" class="form-control" type="number" step="0.01" placeholder="Введите число">
+    <div class="flex">
+        <div class="mr-2">
+            <label for="price" class="form-label">Цена</label>
+            <input id="price"  name="price" class="form-control numeric-input" type="number" step="0.01">
+        </div>
+        <div class="mr-2">
+            <label for="count_bus" class="form-label">Количество компаний</label>
+            <input id="count_bus"  name="count_bus" class="form-control numeric-input" type="number" step="0.01">
+        </div>
     </div>
 </div>
 
-<div class="form-group">
-    <div class="mb-3">
-        <label for="count_bus" class="form-label">Количество компаний</label>
-        <input id="count_bus"  name="count_bus" class="form-control" type="number" step="0.01" placeholder="Введите число">
-    </div>
-</div>
-
-<button type="submit" class="btn btn-primary">Submit</button>
-
-
+<button type="submit" class="btn btn-primary">Сохранить</button>
