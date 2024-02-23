9518da7a11872cc4e0a9b471efb86fdf09abc9d2
Kozavochka <planchet2013@gmail.com>
2024-02-23 22:58:30 +0300

    refactoring stock form

--- resources/views/admin/stocks/edit.blade.php
+++ resources/views/admin/stocks/edit.blade.php
@@ -3,39 +3,40 @@
     <form method="post" action="{{ route('admin.stocks.update', $stock) }}" >
         @csrf
         @method('patch')
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="name" class="form-label">Название</label>
-                <input id="name" name="name"  class="form-control" type="text" placeholder="Введите название"
-                       value="{{ old('name', $stock->name) }}">
-            </div>
+        <div class="form-group mb-3">
+                <div class="row justify-content-start">
+                    <div class="col-3">
+                        <label for="name" class="form-label">Название</label>
+                        <input id="name" name="name"  class="form-control text-input" type="text" placeholder="Введите название"
+                               value="{{ old('name', $stock->name) }}">
+                    </div>
+                    <div class="col-3">
+                        <label for="ticker" class="form-label">Тикер</label>
+                        <input id="ticker" name="ticker"  class="form-control ticker-input" type="text" placeholder="Введите название"
+                               value="{{ old('ticker', $stock->ticker) }}">
+                    </div>
+                </div>
         </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="ticker" class="form-label">Тикер</label>
-                <input id="ticker" name="ticker"  class="form-control" type="text" placeholder="Введите название"
-                       value="{{ old('ticker', $stock->ticker) }}">
+
+        <div class="form-group mb-3">
+            <div class="row justify-content-start">
+                <div class="col-2">
+                    <label for="price" class="form-label">Цена за лот</label>
+                    <input id="price"  name="price" class="form-control numeric-input" type="number" step="0.01" placeholder="Введите число"
+                         value="{{ old('price', $stock->price) }}">
+                    </div>
+                <div class="col-2">
+                    <label for="lots" class="form-label">Количество лотов</label>
+                    <input id="lots"  name="lots" class="form-control numeric-input" type="number" placeholder="Введите число"
+                          value="{{ old('lots', $stock->lots) }}" >
+                </div>
             </div>
         </div>
 
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="price" class="form-label">Цена за лот</label>
-                <input id="price"  name="price" class="form-control" type="number" step="0.01" placeholder="Введите число"
-                       value="{{ old('price', $stock->price) }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="lots" class="form-label">Количество лотов</label>
-                <input id="lots"  name="lots" class="form-control" type="number" placeholder="Введите число"
-                       value="{{ old('lots', $stock->lots) }}" >
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
+        <div class="form-group mb-3">
+            <div>
                 <label for="direction_id" class="form-label">Выбор отрасли</label>
-                <select class="form-select" aria-label="Default select example" id="industry_id" name="industry_id">
+                <select class="form-select text-input" aria-label="Default select example" id="industry_id" name="industry_id">
                     @if(isset($stock->industry->id))
                         <option selected value="{{$stock->industry->id}}" >{{$stock->industry->name}}</option>
                     @endif
--- resources/views/admin/stocks/form.blade.php
+++ resources/views/admin/stocks/form.blade.php
@@ -1,33 +1,34 @@
-<div class="form-group">
-    <div class="mb-3">
-        <label for="name" class="form-label">Название</label>
-        <input id="name" name="name"  class="form-control" type="text" placeholder="Введите название" >
-    </div>
-</div>
-<div class="form-group">
-    <div class="mb-3">
-        <label for="ticker" class="form-label">Тикер</label>
-        <input id="ticker" name="ticker"  class="form-control" type="text" placeholder="Введите название" >
+<div class="form-group mb-3">
+    <div class="row justify-content-start">
+        <div class="col-3">
+            <label for="name" class="form-label">Название</label>
+            <input id="name" name="name"  class="form-control text-input" type="text" placeholder="Введите название">
+        </div>
+        <div class="col-3">
+            <label for="ticker" class="form-label">Тикер</label>
+            <input id="ticker" name="ticker"  class="form-control ticker-input" type="text" placeholder="Введите название">
+        </div>
     </div>
 </div>
 
-<div class="form-group">
-    <div class="mb-3">
-        <label for="price" class="form-label">Цена за лот</label>
-        <input id="price"  name="price" class="form-control" type="number" step="0.01" placeholder="Введите число">
-    </div>
-</div>
-<div class="form-group">
-    <div class="mb-3">
-        <label for="lots" class="form-label">Количество лотов</label>
-        <input id="lots"  name="lots" class="form-control" type="number" placeholder="Введите число">
+<div class="form-group mb-3">
+    <div class="row justify-content-start">
+        <div class="col-2">
+            <label for="price" class="form-label">Цена за лот</label>
+            <input id="price"  name="price" class="form-control numeric-input" type="number" step="0.01" placeholder="Введите число"">
+        </div>
+        <div class="col-2">
+            <label for="lots" class="form-label">Количество лотов</label>
+            <input id="lots"  name="lots" class="form-control numeric-input" type="number" placeholder="Введите число" >
+        </div>
     </div>
 </div>
 
 <div class="form-group">
     <div class="mb-3">
         <label for="direction_id" class="form-label">Выбор отрасли</label>
-        <select class="form-select" aria-label="Default select example" id="industry_id" name="industry_id">
+        <select class="form-select text-input" aria-label="Default select example" id="industry_id" name="industry_id">
+            <option value=""></option>
             @foreach($industries as $industry)
                 <option value="{{$industry->id}}">{{$industry->name}}</option>
             @endforeach
