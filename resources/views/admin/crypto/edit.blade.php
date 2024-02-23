6be83feab6948443aa6aaac0010c312e9822a029
Kozavochka <planchet2013@gmail.com>
2024-02-23 23:28:53 +0300

    refactoring crypto forms

--- resources/views/admin/crypto/edit.blade.php
+++ resources/views/admin/crypto/edit.blade.php
@@ -3,36 +3,36 @@
     <form method="post" action="{{ route('admin.crypto.update', $crypto) }}" >
         @csrf
         @method('patch')
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="name" class="form-label">Название</label>
-                <input id="name" name="name"  class="form-control" type="text" placeholder="Введите название"
+        <div class="form-group mb-3">
+            <div class="flex">
+                <div class="mr-2">
+                    <label for="name" class="form-label">Название</label>
+                    <input id="name" name="name"  class="form-control text-input" type="text" placeholder="Введите название"
                        value="{{ old('name', $crypto->name) }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="ticker" class="form-label">Тикер</label>
-                <input id="ticker" name="ticker"  class="form-control" type="text" placeholder="Введите название"
+                </div>
+                <div class="mr-2">
+                    <label for="ticker" class="form-label">Тикер</label>
+                    <input id="ticker" name="ticker"  class="form-control ticker-input" type="text" placeholder="Тикер"
                        value="{{ old('ticker', $crypto->ticker) }}">
+                </div>
             </div>
         </div>
 
         <div class="form-group">
-            <div class="mb-3">
-                <label for="price" class="form-label">Стоимость</label>
-                <input id="price"  name="price" class="form-control" type="number" step="0.01" placeholder="Введите число"
+            <div class="flex">
+                <div class="mr-2">
+                    <label for="price" class="form-label">Стоимость</label>
+                    <input id="price"  name="price" class="form-control numeric-input" type="number" step="0.01"
                        value="{{ old('price', $crypto->price) }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="mb-3">
-                <label for="lots" class="form-label">Количество лотов</label>
-                <input id="lots"  name="lots" class="form-control" type="number" placeholder="Введите число" step="0.00001"
+                </div>
+                <div class="mr-2">
+                    <label for="lots" class="form-label">Количество лотов</label>
+                    <input id="lots"  name="lots" class="form-control" type="number" step="0.00001"
                        value="{{ old('lots', $crypto->lots) }}" >
+                </div>
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Сохранить</button>
     </form>
 @endsection
