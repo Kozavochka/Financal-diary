0a86b0d2f464bbcdd2101eef2910d211e5849a56
Andrey <planchet2013@example.com>
2024-09-15 00:31:36 +0300

    refactoring front dynamic statistic

--- app/Services/Chart/DataChartServiceContract.php
+++ app/Services/Chart/DataChartServiceContract.php
@@ -4,8 +4,6 @@ namespace App\Services\Chart;
 
 interface DataChartServiceContract
 {
-    const CHUNK_SIZE = 20;
-
     public function getChartData();
 
     public function getAssetStatisticData();
