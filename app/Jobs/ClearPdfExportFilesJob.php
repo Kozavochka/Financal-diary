88ee1bd68a1041ece5c481dbad288c651419a661
Andrey <planchet2013@example.com>
2025-01-07 01:17:33 +0300

    add clear task to scheduler, make scheduler container

---
+++ app/Jobs/ClearPdfExportFilesJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Export\Pdf\AbstractPdfExportService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class ClearPdfExportFilesJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Storage::deleteDirectory(AbstractPdfExportService::STORAGE_DIRECTORY_NAME);
+    }
+}
